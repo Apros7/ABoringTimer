38157080101a4ec2cc1c4b4fe70047a6ef7c4da8
apros7 <lucas.vilsen@gmail.com>
2023-04-19 12:55:58 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ABoringTimer
+A simple timer with some nice music
+Add your own music in a folder called music and then edit line 8 in timer.js.
+
+To make it into an macbook app use:
+´´´
+
+´´´
--- timer.js
+++ timer.js
@@ -5,28 +5,66 @@ function pad(val) {
 var timer;
 var startTime;
 var totalTimer = 0;
+var audio = new Audio("music/jazz.mp3");
+audio.volume = 0;
+
+function music() {
+	if (isPaused) {
+        var interval = setInterval(function() {
+            audio.volume -= 0.01
+            if (audio.volume <= 0.02) {
+				audio.volume = 0
+                audio.pause();
+                clearInterval(interval);
+            }
+        }, 100);
+    }
+	if (isPaused == false) {
+        audio.play();
+        var interval = setInterval(function() {
+            audio.volume += 0.01;
+            if (audio.volume >= 0.1) {
+                clearInterval(interval);
+            }
+        }, 100);
+    }
+}
+
+setInterval(function() {
+	if (totalTimer <= 0.1 && isPaused == false) {
+		start_button.click();
+	}
+}, 100);
 
 function updateTimer(addedTime) {
-	totalTimer += addedTime;
-	console.log(totalTimer)
-	var hours = Math.floor(totalTimer / (1000 * 60 * 60));
-	var minutes = Math.floor((totalTimer % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((totalTimer % (1000 * 60)) / 1000);
-	document.getElementById("timer").innerHTML = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+    totalTimer += addedTime;
+    if (totalTimer < 0) {
+        totalTimer = 0;
+    }
+    var hours = Math.floor(totalTimer / (1000 * 60 * 60));
+    var minutes = Math.floor((totalTimer % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((totalTimer % (1000 * 60)) / 1000);
+    document.getElementById("timer").innerHTML = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
 }
 
 minus_one_min = document.getElementById("minus-one-min")
 minus_fifteen_sec = document.getElementById("minus-fifteen-sec")
 plus_one_min = document.getElementById("plus-one-min")
 plus_fifteen_sec = document.getElementById("plus-fifteen-sec")
+reset = document.getElementById("reset-timer")
+
+reset.addEventListener("click", function() {
+	totalTimer = 0;
+	updateTimer(0);
+})
 
 minus_one_min.addEventListener("click", function() {
-	const remainingTime = 60 * 1000;
+	const remainingTime = -60 * 1000;
 	updateTimer(remainingTime);
 });
 
 minus_fifteen_sec.addEventListener("click", function() {
-	const remainingTime = 15 * 1000;
+	const remainingTime = -15 * 1000;
 	updateTimer(remainingTime);
 });
 
@@ -40,11 +78,53 @@ plus_one_min.addEventListener("click", function() {
 	updateTimer(remainingTime);
 });
 
-document.getElementById("start-timer").addEventListener("click", function() {
-	var timer = setInterval(function() {
-	  updateTimer(-1000);
-	  if (totalTimer <= 0) {
-		clearInterval(timer);
-	  }
+
+start_button = document.getElementById("start-timer")
+timer_buttons = document.getElementById("timer-buttons")
+var isPaused = true;
+
+
+
+function startTimer() {
+	timer = setInterval(function() {
+		updateTimer(-1000);
+		if (totalTimer <= 0) {
+			clearInterval(timer);
+		}
 	}, 1000);
-  });
+}
+
+function pauseTimer() {
+	clearInterval(timer);
+}
+
+start_button.addEventListener("click", function() {
+	if (isPaused) {
+		isPaused = false;
+		startTimer();
+		music();
+		start_button.innerHTML = "Pause";
+		timer_buttons.style.display = "none";
+	} else {
+		isPaused = true;
+		pauseTimer();
+		music();
+		start_button.innerHTML = "Start";
+		timer_buttons.style.display = "flex";
+	}
+});
+
+// Shortcuts
+
+brush_teeth = document.getElementById("brush-teeth");
+stretching = document.getElementById("stretching");
+
+brush_teeth.addEventListener("click", function() {
+	totalTimer = 2*60*1000
+	updateTimer(0);
+})
+
+stretching.addEventListener("click", function() {
+	totalTimer = 15*60*1000
+	updateTimer(0);
+})
